4260fdea8557ad07cc55f90e9195a9b9ab000510
Ryan Tremaine <ryan.trem@gmail.com>
2015-02-07 20:37:27 -0500

    Use jquery.form for contact-us form

--- routes/contact.js
+++ routes/contact.js
@@ -1,20 +1,19 @@
 exports.create = function(postmark){
   return function(req, res){
-    var contact = req.body.contact
-      postmark.send({
-        "From": "info@greenfieldhq.com", 
-        "To": "info@greenfieldhq.com", 
-        "Subject": "Contact Us", 
-        "TextBody": JSON.stringify(contact)
-      }, function(error, success) {
-        if(error) {
-          console.error("Unable to send via postmark: " + error.message);
-          return;
-        }
-        console.info("Sent to postmark for delivery")
-      });
-      res.write('{"contacts":');
-      res.write(JSON.stringify(contact));
-      res.end('}');
+    postmark.send({
+      "From": "info@greenfieldhq.com", 
+      "To": "info@greenfieldhq.com", 
+      "Subject": "Contact Us", 
+      "TextBody": JSON.stringify(req.body)
+    }, function(error, success) {
+      if(error) {
+        console.error("Unable to send via postmark: " + error.message);
+        return;
+      }
+      console.info("Sent to postmark for delivery")
+    });
+    res.write('{"contacts":');
+     res.write(JSON.stringify(req.body));
+    res.end('}');
   }
 };
