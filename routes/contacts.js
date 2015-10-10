2944b9509e96999723b710abc585102c4ef324d6
Eric Kelly <heroiceric@gmail.com>
2015-10-10 13:55:07 -0400

    Upgrade to express 4
    - Updates app to express 4.13.1
    - Replaces konfig with dotenv
    - Removes prerender (wasn't being used)
    - Removes jade templating (wasn't being used)

---
+++ routes/contacts.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const postmark = require('postmark');
+
+const postmarkClient = new postmark.Client(process.env.POSTMARK);
+const router = express.Router();
+
+router.post('/', function(req, res, next) {
+  client.sendEmail({
+    'From': 'info@greenfieldhq.com',
+    'To': 'info@greenfieldhq.com',
+    'Subject': 'Contact Us',
+    'TextBody': JSON.stringify(req.body)
+  }, (error, success) => {
+    if (error) {
+      console.error(`Unable to send via postmark: ${error.message}`);
+      return;
+    }
+
+    console.info('Sent to postmark for delivery')
+  });
+});
+
+module.exports = router;
